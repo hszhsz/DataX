f906dcbd606fc609c4c0d1e7f3787d6526d29fb6
yueshunshun <shunshun.yss@alibaba-inc.com>
2018-08-16 15:43:43 +0800

    add example json

---
+++ cassandrareader/src/main/java/com/alibaba/datax/plugin/reader/cassandrareader/CassandraReaderSplitUtil.java
@@ -0,0 +1,10 @@
+package com.alibaba.datax.plugin.reader.cassandrareader;
+
+/**
+ * Desc:
+ * Mail: shunshun.yss@alibaba-inc.com
+ * Created by yueshunshun
+ * Date: 2018/8/16
+ */
+public class CassandraReaderSplitUtil {
+}
